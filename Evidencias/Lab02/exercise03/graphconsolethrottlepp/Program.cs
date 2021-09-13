21b11fcb1a17802b5fe5be3a5799da79a9e635c5
host4ideas <felixmb99@hotmail.es>
2021-09-13 14:12:34 +0200

    Lab02 exercise02 & 03

---
+++ Evidencias/Lab02/exercise03/graphconsolethrottlepp/Program.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security;
+using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+using Microsoft.Graph;
+using Microsoft.Extensions.Configuration;
+using Helpers;
+using System;
+using Newtonsoft.Json;
+
+namespace graphconsolethrottlepp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = LoadAppSettings();
+            if (config == null)
+            {
+                Console.WriteLine("Invalid appsettings.json file.");
+                return;
+            }
+
+            var userName = ReadUsername();
+            var userPassword = ReadPassword();
+            var client = GetAuthenticatedGraphClient(config, userName, userPassword);
+
+            var stopwatch = new System.Diagnostics.Stopwatch();
+            stopwatch.Start();
+
+            var clientResponse = client.Me.Messages
+                              .Request()
+                              .Select(m => new { m.Id })
+                              .Top(100)
+                              .GetAsync()
+                              .Result;
+
+            var tasks = new List<Task>();
+            foreach (var graphMessage in clientResponse.CurrentPage)
+            {
+                tasks.Add(Task.Run(() =>
+                {
+                    Console.WriteLine("...retrieving message: {0}", graphMessage.Id);
+                    var messageDetail = GetMessageDetail(client, graphMessage.Id);
+                    Console.WriteLine("SUBJECT: {0}", messageDetail.Subject);
+                }));
+            }
+
+            // do all work in parallel & wait for it to complete
+            var allWork = Task.WhenAll(tasks);
+            try
+            {
+                allWork.Wait();
+            }
+            catch
+            {
+                stopwatch.Stop();
+                Console.WriteLine();
+                Console.WriteLine("Elapsed time: {0} seconds", stopwatch.Elapsed.Seconds);
+            }
+        }
+
+        private static IConfigurationRoot LoadAppSettings()
+        {
+            try
+            {
+                var config = new ConfigurationBuilder()
+                                  .SetBasePath(System.IO.Directory.GetCurrentDirectory())
+                                  .AddJsonFile("appsettings.json", false, true)
+                                  .Build();
+                if (string.IsNullOrEmpty(config["applicationId"]) ||
+                    string.IsNullOrEmpty(config["tenantId"]))
+                {
+                    return null;
+                }
+                return config;
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+                return null;
+            }
+        }
+
+        private static IAuthenticationProvider CreateAuthorizationProvider(IConfigurationRoot config, string userName, SecureString userPassword)
+        {
+            var clientId = config["applicationId"];
+            var authority = $"https://login.microsoftonline.com/{config["tenantId"]}/v2.0";
+            List<string> scopes = new List<string>();
+            scopes.Add("User.Read");
+            scopes.Add("Mail.Read");
+            var cca = PublicClientApplicationBuilder.Create(clientId)
+                                                    .WithAuthority(authority)
+                                                    .Build();
+            return MsalAuthenticationProvider.GetInstance(cca, scopes.ToArray(), userName, userPassword);
+        }
+
+        private static GraphServiceClient GetAuthenticatedGraphClient(IConfigurationRoot config, string userName, SecureString userPassword)
+        {
+            var authenticationProvider = CreateAuthorizationProvider(config, userName, userPassword);
+            var graphClient = new GraphServiceClient(authenticationProvider);
+            return graphClient;
+        }
+
+        private static SecureString ReadPassword()
+        {
+            Console.WriteLine("Enter your password");
+            SecureString password = new SecureString();
+            while (true)
+            {
+                ConsoleKeyInfo c = Console.ReadKey(true);
+                if (c.Key == ConsoleKey.Enter)
+                {
+                    break;
+                }
+                password.AppendChar(c.KeyChar);
+                Console.Write("*");
+            }
+            Console.WriteLine();
+            return password;
+        }
+
+        private static string ReadUsername()
+        {
+            string username;
+            Console.WriteLine("Enter your username");
+            username = Console.ReadLine();
+            return username;
+        }
+
+        private static Microsoft.Graph.Message GetMessageDetail(GraphServiceClient client, string messageId)
+        {
+            // submit request to Microsoft Graph & wait to process response
+            return client.Me.Messages[messageId].Request().GetAsync().Result;
+        }
+    }
+}
